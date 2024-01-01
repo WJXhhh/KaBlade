a0a5fe374d46539e20891cd2a7fde0ec4bff20fe
WJXhhh <miladuo43@163.com>
2024-01-01 09:31:41 +0800

    update1.5.3

--- src/main/java/com/wjx/kablade/mixin/MixinModDiscoverer.java
+++ src/main/java/com/wjx/kablade/mixin/MixinModDiscoverer.java
@@ -1,6 +1,6 @@
 package com.wjx.kablade.mixin;
 
-import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import net.minecraftforge.fml.common.ModContainer;
 import net.minecraftforge.fml.common.discovery.ModDiscoverer;
 import org.spongepowered.asm.mixin.Mixin;
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 
@@ -18,26 +18,30 @@ public class MixinModDiscoverer {
     @Inject(method = "identifyMods", at = @At(value = "RETURN"),locals = LocalCapture.CAPTURE_FAILSOFT, remap = false)
     private void injectBeforeReturn(CallbackInfoReturnable<List<ModContainer>> cir,List<ModContainer> modList) {
         String id = "kablade";
+        HashMap<String,Integer> maps = Maps.newHashMap();
         int count = 0;
-        boolean flag = false;
-        for (ModContainer c1 : modList){
-            if (c1.getModId().equals("networkmod")){
-                flag = true;
+        for (ModContainer c : modList) {
+            if (c.getModId().equals(id)) {
+                if (!maps.containsKey(c.getSource().getName())){
+                    maps.put(c.getSource().getName(),1);
+                }
+                else maps.put(c.getSource().getName(),maps.get(c.getSource().getName())+1);
+
+                //count++;
             }
         }
-        if (flag){
-            for (ModContainer c : modList){
-                if (c.getModId().equals(id)){
-                    count++;
-                }
+        String name = null;
+        for (String k : maps.keySet()){
+            if (maps.get(k)>1){
+                name = k;
             }
-            if (count >=2){
-                for (Iterator<ModContainer> it = modList.iterator();it.hasNext();){
-                    ModContainer c = it.next();
-                    if (c.getModId().equals(id)){
-                        it.remove();
-                        break;
-                    }
+        }
+        if (name!=null){
+            for (Iterator<ModContainer> it = modList.iterator();it.hasNext();){
+                ModContainer c = it.next();
+                if (c.getModId().equals(id)&&c.getSource().getName().equals(name)){
+                    it.remove();
+                    break;
                 }
             }
         }
