a8bd8eb0c784662962ad3137459aa7255e56ff83
WJXhhh <miladuo43@163.com>
2024-02-03 19:52:50 +0800

    add SEPowerOfWind

--- src/main/java/com/wjx/kablade/SlashBlade/SpeacialEffects/SEPowerOfWind.java
+++ src/main/java/com/wjx/kablade/SlashBlade/SpeacialEffects/SEPowerOfWind.java
@@ -1,15 +1,20 @@
 package com.wjx.kablade.SlashBlade.SpeacialEffects;
 
+import com.wjx.kablade.SlashBlade.BladeProxy;
+import com.wjx.kablade.util.BladeAttackEventManager;
 import mods.flammpfeil.slashblade.specialeffect.ISpecialEffect;
 import mods.flammpfeil.slashblade.specialeffect.SpecialEffects;
 import mods.flammpfeil.slashblade.specialeffect.IRemovable;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.attributes.IAttribute;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 
-
-
-
-
-public class SEPowerOfWind implements ISpecialEffect,Removeable{
+public class SEPowerOfWind implements ISpecialEffect,IRemovable{
 
     @Override
     public boolean canCopy(ItemStack itemStack) {
@@ -24,7 +29,7 @@ public class SEPowerOfWind implements ISpecialEffect,Removeable{
     @Override
     public void register() {
         MinecraftForge.EVENT_BUS.register(this);
-        BladeAttackEventManager.addEvent(EMInduction);
+        //BladeAttackEventManager.addEvent(_);
     }
 
     @Override
@@ -42,10 +47,12 @@ public class SEPowerOfWind implements ISpecialEffect,Removeable{
     public void updateSEATKImprove(TickEvent.PlayerTickEvent event){
 
         if(event.phase==TickEvent.Phase.START){
-            if (SpecialEffects.isEffective(player,stack, BladeProxy.PowerOfWind) == SpecialEffects.State.Effective){
-                //player.getAttributeModifierAmount
-
-
+            if (SpecialEffects.isEffective(event.player, event.player.getHeldItemMainhand(), BladeProxy.PowerOfWind) == SpecialEffects.State.Effective){
+                EntityPlayer player= event.player;
+                ItemStack stack=event.player.getHeldItemMainhand();
+                double speed=player.getAttributeMap().getAttributeInstance(SharedMonsterAttributes.MOVEMENT_SPEED).getBaseValue();
+                double damage=player.getAttributeMap().getAttributeInstance(SharedMonsterAttributes.ATTACK_DAMAGE).getBaseValue();
+                player.getAttributeMap().getAttributeInstance(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(damage+speed/10);
 
 
 
