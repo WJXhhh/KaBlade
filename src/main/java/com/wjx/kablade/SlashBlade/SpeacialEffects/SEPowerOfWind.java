ececcf6a381cd8ffc00c94fd1ec8cb6abaa5b633
WJXhhh <miladuo43@163.com>
2024-02-03 20:41:00 +0800

    change se

--- src/main/java/com/wjx/kablade/SlashBlade/SpeacialEffects/SEPowerOfWind.java
+++ src/main/java/com/wjx/kablade/SlashBlade/SpeacialEffects/SEPowerOfWind.java
@@ -9,12 +9,17 @@ import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.attributes.*;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 
+import java.util.UUID;
+
 
 public class SEPowerOfWind implements ISpecialEffect,IRemovable{
+
+    public UUID powAttid = UUID.fromString("739B518D-F9EF-04F7-AD8E-98AE7D3C5FE8");
 
     @Override
     public boolean canCopy(ItemStack itemStack) {
@@ -50,12 +55,14 @@ public class SEPowerOfWind implements ISpecialEffect,IRemovable{
             if (SpecialEffects.isEffective(event.player, event.player.getHeldItemMainhand(), BladeProxy.PowerOfWind) == SpecialEffects.State.Effective){
                 EntityPlayer player= event.player;
                 ItemStack stack=event.player.getHeldItemMainhand();
+                NBTTagCompound origin = player.nbt
                 double speed=player.getAttributeMap().getAttributeInstance(SharedMonsterAttributes.MOVEMENT_SPEED).getBaseValue();
                 double damage=player.getAttributeMap().getAttributeInstance(SharedMonsterAttributes.ATTACK_DAMAGE).getBaseValue();
                 AbstractAttributeMap map = player.getAttributeMap();
                 IAttributeInstance instance = map.getAttributeInstance(SharedMonsterAttributes.ATTACK_DAMAGE);
-                instance.applyModifier(new AttributeModifier());
+                instance.applyModifier(new AttributeModifier(powAttid,"pow_att",speed/10,0));
                 player.getAttributeMap().getAttributeInstance(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(damage+speed/10);
+
 
 
 
