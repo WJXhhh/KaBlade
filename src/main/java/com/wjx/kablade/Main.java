712f9258fbf7c05adfa7e28277cd8d27fb82a1e5
WJXhhh <miladuo43@163.com>
2024-02-06 11:13:34 +0800

    change ver

--- src/main/java/com/wjx/kablade/Main.java
+++ src/main/java/com/wjx/kablade/Main.java
@@ -42,7 +42,7 @@ public class Main
 {
     public static final String MODID = "kablade";
     public static final String NAME = "Ka Blades";
-    public static final String VERSION = "1.5.4";
+    public static final String VERSION = "1.6.0";
 
     public static final SimpleNetworkWrapper PACKET_HANDLER = NetworkRegistry.INSTANCE.newSimpleChannel("kablade");
 
