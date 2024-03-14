7af9d6f9981c862bb8ada81f821e33d71a873f1c
WJXhhh <72961648+WJXhhh@users.noreply.github.com>
2024-03-14 10:12:37 +0800

    changever

--- src/main/java/com/wjx/kablade/Main.java
+++ src/main/java/com/wjx/kablade/Main.java
@@ -46,7 +46,7 @@ public class Main
 {
     public static final String MODID = "kablade";
     public static final String NAME = "Ka Blades";
-    public static final String VERSION = "1.6.3";
+    public static final String VERSION = "1.6.4";
 
     public static final boolean EnableAllWeapon =false;
 
