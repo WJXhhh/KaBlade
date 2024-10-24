9bf239bf7cefd59017f0ab9f7e03e2a335faaa06
WJXhhh <72961648+WJXhhh@users.noreply.github.com>
2024-10-24 17:08:52 +0800

    yh

--- src/main/java/com/wjx/kablade/util/KaBladePlayerProp.java
+++ src/main/java/com/wjx/kablade/util/KaBladePlayerProp.java
@@ -3,7 +3,6 @@ package com.wjx.kablade.util;
 import com.google.common.collect.Lists;
 import com.wjx.kablade.Main;
 import com.wjx.kablade.network.MessageUpdateKaBladePlayerProp;
-import javafx.util.Pair;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
 import scala.Int;
