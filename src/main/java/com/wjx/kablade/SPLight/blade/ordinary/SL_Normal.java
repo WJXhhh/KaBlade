776d6fe9e9f0cc3bf85b3adfde409b6e3bf2e547
WJXhhh <miladuo43@163.com>
2025-05-05 10:47:37 +0800

    feat(SPLight): 新增普通刀blade并优化特殊效果
    - 新增普通刀blade的实现类SL_Normal
    - 优化SPLighting特殊效果，增加更新事件处理和使用条件判断
    -调整实体选择逻辑，提高性能和可读性

---
+++ src/main/java/com/wjx/kablade/SPLight/blade/ordinary/SL_Normal.java
@@ -0,0 +1,55 @@
+package com.wjx.kablade.SPLight.blade.ordinary;
+
+import com.wjx.kablade.AllWeapon.blade.items.Item_AwNamed;
+import com.wjx.kablade.SlashBlade.BladeLoader;
+import mods.flammpfeil.slashblade.ItemSlashBladeNamed;
+import mods.flammpfeil.slashblade.SlashBlade;
+import mods.flammpfeil.slashblade.item.ItemSlashBlade;
+import mods.flammpfeil.slashblade.named.event.LoadEvent;
+import mods.flammpfeil.slashblade.util.SlashBladeHooks;
+import net.minecraft.init.Enchantments;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+public class SL_Normal {
+    String name = "wjx.splight.normal";
+
+    public SL_Normal(){
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private ItemStack customblade = new ItemStack(BladeLoader.ITEM_SL,1,0);
+
+    private NBTTagCompound Tag = new NBTTagCompound();
+    {
+        customblade.setTagCompound(Tag);
+    }
+
+    @SubscribeEvent
+    public void init(LoadEvent.InitEvent event){
+        Item_AwNamed.CurrentItemName.set(Tag,name);
+        Item_AwNamed.CustomMaxDamage.set(Tag,60);
+        ItemSlashBlade.ModelName.set(Tag,"named/sange/sange");
+        ItemSlashBlade.TextureName.set(Tag, "splight/splight/normal/texture");
+        //ItemSlashBlade.IsNoScabbard.set(Tag,true);
+        ItemSlashBladeNamed.IsDefaultBewitched.set(Tag, true);
+        ItemSlashBlade.BaseAttackModifier.set(Tag,8f);
+        customblade.addEnchantment(Enchantments.LOOTING,1);
+        customblade.addEnchantment(Enchantments.POWER,3);
+        customblade.addEnchantment(Enchantments.UNBREAKING,1);
+        customblade.addEnchantment(Enchantments.SHARPNESS,3);
+        //customblade.addEnchantment(Enchantments.FIRE_ASPECT,20);
+        //customblade.addEnchantment(Enchantments.POWER,20);
+        //customblade.addEnchantment(Enchantments.,20);
+        SlashBlade.registerCustomItemStack(name,customblade);
+        BladeLoader.SLBlades.add(name);
+        //AchievementList.registerAchievement("name",customblade,null);
+    }
+
+    @SubscribeEvent
+    public void postinit(LoadEvent.PostInitEvent event) {
+        SlashBladeHooks.EventBus.register(this);
+    }
+}
--- src/main/java/com/wjx/kablade/SlashBlade/SpeacialEffects/SPLight/SPLighting.java
+++ src/main/java/com/wjx/kablade/SlashBlade/SpeacialEffects/SPLight/SPLighting.java
@@ -3,17 +3,29 @@ package com.wjx.kablade.SlashBlade.SpeacialEffects.SPLight;
 import com.wjx.kablade.ExSA.entity.EntityLightningSword;
 import com.wjx.kablade.ExSA.entity.EntityPhantomSwordEx;
 import mods.flammpfeil.slashblade.ability.StylishRankManager;
+import mods.flammpfeil.slashblade.entity.selector.EntitySelectorAttackable;
 import mods.flammpfeil.slashblade.item.ItemSlashBlade;
 import mods.flammpfeil.slashblade.specialeffect.ISpecialEffect;
+import mods.flammpfeil.slashblade.specialeffect.SpecialEffects;
+import mods.flammpfeil.slashblade.util.SlashBladeEvent;
 import mods.flammpfeil.slashblade.util.SlashBladeHooks;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Enchantments;
+import net.minecraft.init.MobEffects;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import java.util.List;
 
 public class SPLighting implements ISpecialEffect {
     @Override
@@ -31,78 +43,119 @@ public class SPLighting implements ISpecialEffect {
         return "SPLighting";
     }
 
-    /*
+    protected int SPLighting = 1;
+
+    public void setExLightingLevel(int level) {
+        this.SPLighting = level;
+    }
+
+    private boolean useBlade(ItemSlashBlade.ComboSequence sequence) {
+        if (sequence.useScabbard) {
+            return false;
+        }
+        if (sequence == ItemSlashBlade.ComboSequence.None) {
+            return false;
+        }
+        return sequence != ItemSlashBlade.ComboSequence.Noutou;
+    }
+
+    @SubscribeEvent
+    public void onUpdateItemSlashBlade(SlashBladeEvent.OnUpdateEvent event) {
+        int check;
+        if (!SpecialEffects.isPlayer(event.entity)) {
+            return;
+        }
+        EntityPlayer player = (EntityPlayer)event.entity;
+        ItemStack blade = event.blade;
+        NBTTagCompound tag = ItemSlashBlade.getItemTagCompound(blade);
+        if (ItemSlashBlade.IsBroken.get(tag)) {
+            return;
+        }
+        switch (SpecialEffects.isEffective(player, event.blade, this).ordinal()) {
+            case 0: {
+                return;
+            }
+            case 1: {
+                return;
+            }
+            case 2: {
+                double d0 = player.getRNG().nextGaussian() * 0.02;
+                double d1 = player.getRNG().nextGaussian() * 0.02;
+                double d2 = player.getRNG().nextGaussian() * 0.02;
+                double d3 = 10.0;
+                event.world.spawnParticle(EnumParticleTypes.SPELL_WITCH, player.posX + (double)(player.getRNG().nextFloat() * player.width * 2.0f) - (double)player.width - d0 * d3, player.posY, player.posZ + (double)(player.getRNG().nextFloat() * player.width * 2.0f) - (double)player.width - d2 * d3, d0, d1, d2);
+            }
+        }
+        ItemSlashBlade.ComboSequence seq = ItemSlashBlade.getComboSequence(tag);
+        if (!this.useBlade(seq)) {
+            return;
+        }
+        PotionEffect haste = player.getActivePotionEffect(MobEffects.HASTE);
+        check = haste != null ? (haste.getAmplifier() != 1 ? 3 : 4) : 2;
+        if (player.swingProgressInt != check) {
+            return;
+        }
+        this.doAddAttack(event.blade, player, seq);
+    }
+
     public void doAddAttack(ItemStack stack, EntityPlayer player, ItemSlashBlade.ComboSequence setCombo) {
+        Entity tmp;
         NBTTagCompound tag = ItemSlashBlade.getItemTagCompound(stack);
         World world = player.world;
         int cost = -2;
-        if (!ItemSlashBlade.ProudSoul.tryAdd(tag, Integer.valueOf(cost), false)) {
+        if (!ItemSlashBlade.ProudSoul.tryAdd(tag, cost, false)) {
             ItemSlashBlade.damageItem(stack, 5, player);
-        } else {
-            Entity target = null;
-            int entityId = ItemSlashBlade.TargetEntityId.get(tag);
-            if (entityId != 0) {
-                Entity tmp = world.getEntityByID(entityId);
-                if (tmp != null && tmp.getDistance(player) < 30.0F) {
-                    target = tmp;
-                }
+            return;
+        }
+        Entity target = null;
+        int entityId = ItemSlashBlade.TargetEntityId.get(tag);
+        if (entityId != 0 && (tmp = world.getEntityByID(entityId)) != null && tmp.getDistance(player) < 30.0f) {
+            target = tmp;
+        }
+        if (target == null) {
+            target = this.getEntityToWatch(player);
+        }
+        if (target != null && !target.isDead && !world.isRemote) {
+            EntityLightningSword entityDrive;
+            float baseModif = ((ItemSlashBlade)stack.getItem()).getBaseAttackModifiers(tag);
+            int level = EnchantmentHelper.getEnchantmentLevel(Enchantments.POWER, stack);
+            float magicDamage = baseModif * (float)(level / 20);
+            int rank = StylishRankManager.getStylishRank(player);
+            if (5 <= rank) {
+                magicDamage *= 2.0f;
             }
-
-            if (target == null) {
-                target = this.getEntityToWatch(player);
+            if ((entityDrive = new EntityLightningSword(world, player, magicDamage, 90.0f)) != null) {
+                entityDrive.setColor(16766720);
+                entityDrive.setInterval(7);
+                entityDrive.setLifeTime(15);
+                entityDrive.setRoll(90.0f);
+                entityDrive.setTargetEntityId(target.getEntityId());
+                world.spawnEntity(entityDrive);
             }
-
-            if (target != null && !target.isDead && !world.isRemote) {
-                float baseModif = ((ItemSlashBlade)stack.getItem()).getBaseAttackModifiers(tag);
-                int level = EnchantmentHelper.getEnchantmentLevel(Enchantments.POWER, stack);
-                float magicDamage = baseModif * (float)(level / 20);
-                int rank = StylishRankManager.getStylishRank(player);
-                if (5 <= rank) {
-                    magicDamage *= 2.0F;
-                }
-
-                EntityPhantomSwordEx entityDrive = new EntityLightningSword(world, player, magicDamage, 90.0F);
-                if (entityDrive != null) {
-                    entityDrive.setColor(16766720);
-                    entityDrive.setInterval(7);
-                    entityDrive.setLifeTime(15);
-                    entityDrive.setRoll(90.0F);
-                    entityDrive.setTargetEntityId(target.getEntityId());
-                    world.spawnEntity(entityDrive);
-                }
-            }
-
         }
     }
 
     private Entity getEntityToWatch(EntityPlayer player) {
-        World world = player.worldObj;
+        World world = player.world;
         Entity target = null;
+        for (int dist = 2; dist < 20; dist += 2) {
 
-        for(int dist = 2; dist < 20; dist += 2) {
-            AxisAlignedBB bb = player.boundingBox.copy();
-            Vec3 vec = player.getLookVec();
-            vec = vec.normalize();
-            bb = bb.expand(2.0D, 0.25D, 2.0D);
-            bb = bb.offset(vec.xCoord * (double)dist, vec.yCoord * (double)dist, vec.zCoord * (double)dist);
-            List<Entity> list = world.getEntitiesWithinAABBExcludingEntity(player, bb, ItemSlashBlade.AttackableSelector);
-            float distance = 30.0F;
-            Iterator var9 = list.iterator();
+            AxisAlignedBB bb = player.getEntityBoundingBox();
+            bb = bb.grow(4.0D, 0.0D, 4.0D);
+            bb = bb.offset(player.motionX, player.motionY, player.motionZ);
 
-            while(var9.hasNext()) {
-                Entity curEntity = (Entity)var9.next();
-                float curDist = curEntity.getDistanceToEntity(player);
-                if (curDist < distance) {
-                    target = curEntity;
-                    distance = curDist;
-                }
+            List<Entity> list = world.getEntitiesInAABBexcluding(player, bb, EntitySelectorAttackable.getInstance());
+            float distance = 30.0f;
+            for (Entity curEntity : list) {
+                float curDist = curEntity.getDistance(player);
+                if (!(curDist < distance)) continue;
+                target = curEntity;
+                distance = curDist;
             }
-
-            if (target != null) {
-                break;
-            }
+            if (target != null) break;
         }
-
         return target;
-    }*/
+    }
+
+
 }
