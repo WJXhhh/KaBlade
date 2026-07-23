03d02ec0e44b4f04f390e0ae748f46cf182e749d
WJXhhh <miladuo43@163.com>
2026-07-23 20:15:45 +0800

    Fix Warm

--- src/main/java/com/wjx/kablade/Main.java
+++ src/main/java/com/wjx/kablade/Main.java
@@ -55,7 +55,7 @@ public class Main
 {
     public static final String MODID = "kablade";
     public static final String NAME = "Ka Blades";
-    public static final String VERSION = "1.9.3";
+    public static final String VERSION = "1.9.4";
 
     public static boolean EnableAllWeapon =true;
 
